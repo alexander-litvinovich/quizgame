12060f8afac6c56d025e66217d3e91666fbeed6b
Alexander Litvinovich <alex@Alexanders-iMac.local>
2018-12-27 00:28:43 +0300

    e

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import outerData from './datamock.json';
 import Card from './components/Card/Card';
 
 class App extends Component {
+
+  questions = {};
+
+  componentDidMount = () => {
+    //console.log(outerData);
+  }
+
   render() {
+    console.log(this.questions);
     return (
       <div className="App">
-        <Card question="Да или нет?" description="Дай четкий ответ!"></Card>
+        <Card question={1+1} description="Дай четкий ответ!"></Card>
         <Card question="Соль или сахар?" description="Дай четкий ответ!"></Card>
         <Card question="Один или ноль?" description="Дай четкий ответ!"></Card>
         <Card />
