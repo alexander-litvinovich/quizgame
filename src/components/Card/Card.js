97ce61f9e3f4758abb13403e57db453e44c8e490
Alexander Litvinovich <alex@Alexanders-iMac.local>
2019-01-21 08:58:59 +0300

    swipe ability added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Swipeable from 'react-swipy';
 import './Card.css';
 
+
 export class Card extends Component {
+  onClickRight  = ()=>{
+    this.forceSwipeRight();
+    this.props.right();
+  }
+  onClickSkip = ()=>{
+    this.forceSwipeLeft();
+    this.props.skip();
+  }
+
+  swipeHandler = (...rest)=>{
+    
+    console.log(rest);
+  }
+  
+  afterSwipeHandler = (...rest)=>{
+    
+    console.log(rest);
+  }
+
+  forceSwipeLeft;
+  forceSwipeRight;
+  
+
   render() {
     const {word, tabooWords, category, image} = this.props;
     return (
-      <div className="Card">
-        <h2>{word}</h2>
-        <div className="tabooWords">
-        <span>you shouldn't say:</span>
-          {tabooWords.map((tabooWord, i)=>{return <div key={i}>{tabooWord}</div>})}
-        </div>
-        {
-          image 
+      <Swipeable
+        onSwipe={this.swipeHandler}
+        onAfterSwipe={this.afterSwipeHandler}
+        buttons={({left, right}) => {
+          this.forceSwipeLeft = left;
+          this.forceSwipeRight = right;
+        }}
+      >
+        <div className="Card">
+          <h2>{word}</h2>
+          <div className="tabooWords">
+          <span>you shouldn't say:</span>
+            {tabooWords.map((tabooWord, i)=>{return <div key={i}>{tabooWord}</div>})}
+          </div>
+          {
+            image 
             ? < img alt="" src = {image}/>
             : ('')
-        }
-        <p>{category}</p>
-        <div>
-            <button name="wrong" onClick={this.props.wrong}><span role="img" aria-label="wrong">👎</span></button>
-            <button name="right" onClick={this.props.right}><span role="img" aria-label="right">👍</span></button>
-            <button name="skip" onClick={this.props.skip}><span role="img" aria-label="skip">👉</span></button>
+          }
+          <p>{category}</p>
+          <div>
+              <button name="wrong" onClick={this.props.wrong}><span role="img" aria-label="wrong">👎</span></button>
+              <button name="right" onClick={this.onClickRight}><span role="img" aria-label="right">👍</span></button>
+              <button name="skip" onClick={this.onClickSkip}><span role="img" aria-label="skip">👉</span></button>
+          </div>
         </div>
-      </div>
+      </Swipeable>
     )
   }
 }
